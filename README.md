5d5612177e6900b78855b462ecfb07d5f99cea61
Shane Bodimer <shanefbodimer@gmail.com>
2024-10-13 18:15:26 -0400

    clean up

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
-# live-censor
-Highlight censored words on Genius
+# No Dump
+
+Highlight bad words on Genius
+
+## Why
+
+When DJing on college radio, we can play any song we want as long as we censor the curse words. Usually, this is done by the radio DJ following along with the lyrics from Genius and hitting the mute button when a bad word comes up.
+
+It is hard to read lyrics, listen to the music, see a bad word come up, and time the mute button all at once.
+
+Sometimes, when reading, bad words slip past and you miss the mute!
+
+To fix this, I created a chrome extension that clearly highlights curse words (or any words really) on Genius, making it 1000000% easier for radio DJs to follow along with a song's lyrics and know when to mute.
+
+This is what it looks like:
+![](./pic.png)
+
+## Set up
+
+- pull this repo locally
+- go to your chrome extension setting page and click "load unpacked"
+- select the repo folder
+- done - now any genius lyric page will have bad words highlighted
+
+To change the word list, go to the extension options page and add any words you want. The initial list of words was picked from Google's profanity list: https://raw.githubusercontent.com/coffee-and-fun/google-profanity-words/refs/heads/main/data/en.txt
+
+![](./options.png)
--- script.js
+++ script.js
@@ -3,41 +3,51 @@
 // Put the bad words back but highlight them
 
 // Get options
-chrome.storage.sync.get("words", function(response) {
+chrome.storage.sync.get("words", function (response) {
   // Place info
-  var lyrics = document.getElementsByClassName("song_body-lyrics");
-  let alert = `<div style="margin-top: 20px; border-radius: 4px; background-color: #F8D7DA; color: #721C24; border: 1px solid #F5C6CB; padding: 12px 20px 12px 20px; display: flex; justify-content: space-between; align-items: center;">Indecent words are highlighted. <button id="go-to-options">Click for options</button></div>`;
-  lyrics[0].innerHTML = alert + lyrics[0].innerHTML;
+  var lyrics = document.querySelectorAll('[data-lyrics-container$="true"]');
+  console.log(lyrics);
+  // let alert = `<div style="margin-top: 20px; border-radius: 4px; background-color: #F8D7DA; color: #721C24; border: 1px solid #F5C6CB; padding: 12px 20px 12px 20px; display: flex; justify-content: space-between; align-items: center;">Indecent words are highlighted. <button id="go-to-options">Click for options</button></div>`;
+  // lyrics[0].innerHTML = alert + lyrics[0].innerHTML;
 
   // Get bad word list
   let badWords = response.words;
 
   // Select element on page to edit
-  var lyrics = document.getElementsByClassName("song_body-lyrics");
-  raw = lyrics[0].innerHTML; // Grab raw html
-  console.log(raw);
+  var lyrics = document.querySelectorAll('[data-lyrics-container$="true"]');
 
-  // Replace all instances of bad word
-  for (i = 0; i < badWords.length; i++) {
-    // Make global regex to find all occurances
-    re = new RegExp(badWords[i], "gi");
-    raw = raw.replace(
-      re,
-      `<span style="background-color: rgba(255, 0, 0, 1); color: #fff; padding: 5px; font-weight: bold; border-radius: 4px;">${
-        badWords[i]
-      }</span>`
-    );
+  lyrics.forEach((element) => {});
+  for (let j = 0; j < lyrics.length; j++) {
+    console.log(lyrics);
+    raw = lyrics[j].innerHTML; // Grab raw html
+    console.log(raw);
+
+    // Replace all instances of bad word
+    for (i = 0; i < badWords.length; i++) {
+      // Make global regex to find all occurances
+      re = new RegExp(" " + badWords[i], "gi");
+      raw = raw.replace(
+        re,
+        `<span style="background-color: rgba(255, 0, 0, 1); color: #fff; padding: 5px; font-weight: bold; border-radius: 4px;">${badWords[i]}</span>`
+      );
+
+      re = new RegExp(badWords[i] + " ", "gi");
+      raw = raw.replace(
+        re,
+        `<span style="background-color: rgba(255, 0, 0, 1); color: #fff; padding: 5px; font-weight: bold; border-radius: 4px;">${badWords[i]}</span>`
+      );
+    }
+
+    // Replace HTML with censored version
+    lyrics[j].innerHTML = raw;
   }
 
-  // Replace HTML with censored version
-  lyrics[0].innerHTML = raw;
-
   // Take user to options
-  document.querySelector("#go-to-options").addEventListener(function() {
-    if (chrome.runtime.openOptionsPage) {
-      chrome.runtime.openOptionsPage();
-    } else {
-      window.open(chrome.runtime.getURL("options.html"));
-    }
-  });
+  // document.querySelector("#go-to-options").addEventListener(function () {
+  //   if (chrome.runtime.openOptionsPage) {
+  //     chrome.runtime.openOptionsPage();
+  //   } else {
+  //     window.open(chrome.runtime.getURL("options.html"));
+  //   }
+  // });
 });
